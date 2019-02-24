afda86b7d30ee55bfa93247d39f4413fa9d1b9f3
Ivan Lim <aznlim17@gmail.com>
2019-02-24 13:52:34 -0700

    Profile Screen Skeleton

--- walkandtalk/src/screens/ProfileScreen.js
+++ walkandtalk/src/screens/ProfileScreen.js
@@ -12,41 +12,104 @@ class ProfileScreen extends Component {
   render() {
     return (
       <ScrollView>
+
         <View style={styles.container}>
           <View style={styles.headerContent}>
+          <View style={styles.edit}>
+            <Image style={styles.editIcon} source={require('../assets/icons/edit.png')}/>
+          </View>
             <Image style={styles.avatar} source={require('../assets/icons/profile.png')}/>
               <Text style={styles.name}>Brittany Taylor</Text>
           </View>
+
           <View style={styles.seperator}/>
-          <View style={styles.body}>
-            <View style={styles.bodyContent}>
-              <Text style={styles.textInfo}>
-                <Text style={styles.textHeader}>Basic Info{"\n"}{"\n"}</Text>
-                <Text>Date of Birth{"\n"}{"\n"}</Text>
-                <Text>Age{"\n"}{"\n"}</Text>
-                <Text>Menopausal Stage{"\n"}</Text>
-              </Text>
-            </View>
-            <View style={styles.seperator}/>
-            <View style={styles.bodyContent}>
-            <Text style={styles.textInfo}>
-                <Text style={styles.textHeader}>My Preferences{"\n"}{"\n"}</Text>
-                <Text>Length of Walk (by distance){"\n"}{"\n"}</Text>
-                <Text>Length of Walk (by duration){"\n"}{"\n"}</Text>
-                <Text>Intensity{"\n"}{"\n"}</Text>
-                <Text>Type of Venue{"\n"}{"\n"}</Text>
-                <Text>Location{"\n"}</Text>
-              </Text>
+
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textHeader}>Basic Info</Text>
             </View>
           </View>
-            <View style={{ flexDirection: 'row' }}>
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Help Line Links</Text>  
-              </TouchableOpacity>              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Past Events</Text> 
-              </TouchableOpacity>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Date of Birth</Text>
             </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>January 1, 1955</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Age</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>64</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Menopausal Stage</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>Peri</Text>
+            </View>
+          </View>
+
+          <View style={styles.seperator}/>
+
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textHeader}>My Preferences</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Length of Walk (by distance)</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>10 km</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Length of Walk (by duration)</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>60 min</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Intensity</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>Intermediate</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Type of Venue</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>Indoor</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInfoStyle}>Location</Text>
+            </View>
+            <View style={styles.textInfo}>
+              <Text style={styles.textInput}>Riverbend Area</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Help Line Links</Text>  
+            </TouchableOpacity>              
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Past Events</Text> 
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     );
@@ -55,7 +118,7 @@ class ProfileScreen extends Component {
 
 const styles = StyleSheet.create({
   headerContent: {
-    padding:20,
+    padding: 20,
     alignItems: 'center',
     marginTop: 40
   },
@@ -64,37 +127,56 @@ const styles = StyleSheet.create({
     height: 80,
     borderRadius: 60,
     borderWidth: 3,
-    borderColor: "black",
+    borderColor: 'black',
     marginBottom: 10,
+  },
+  edit: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 100,
+  },
+  editIcon:{
+    height: 30,
+    width: 30,
+    marginRight: 20
   },
   seperator: {
     borderBottomColor: 'gray',
     borderBottomWidth: 0.5,
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
+    marginBottom: 5,
+    marginTop: 5
+  },
+  row: {
+    flex: 1, 
+    flexDirection: 'row'
+  },
+  textInfo: {
+    flex: 1, 
+    marginLeft: 15, 
+    marginBottom: 5
   },
   name: {
     fontSize: 20,
     color: 'black',
     fontWeight:'500',
   },
-  bodyContent: {
-    flex: 1,
-    alignItems: 'center',
-    padding:30,
-  },
   textHeader: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    fontSize: 13,
+    color: 'gray',
   },
-  textInfo: {
+  textInfoStyle: {
     fontSize: 13,
     color: "gray",
+
   },
-  bodyContent: {
-    paddingTop: 10,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginLeft: 15
+  textInput: {
+    textAlign: 'right',
+    marginRight: 15,
+    color: 'black'
+
   },
   buttonContainer: {
     marginTop: 10,
@@ -103,14 +185,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
-    width:150,
+    width: 150,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: 'black',
     marginLeft: 15,
     marginRight: 10
   },
+
 });
-  
 
 export default ProfileScreen;
