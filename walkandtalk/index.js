81618b7a3669cc47872f831451fdd3b449cead54
Ivan Lim <aznlim17@gmail.com>
2019-02-28 10:09:45 -0700

    Added Radio Buttons, Comments, and General Styling on Edit Profile

--- walkandtalk/index.js
+++ walkandtalk/index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from "react-native";
+import { AppRegistry, YellowBox } from "react-native";
 import App from "./App";
-
+YellowBox.ignoreWarnings(["Require cycle:", "Remote debugger"]);
 AppRegistry.registerComponent("walkandtalk", () => App);
