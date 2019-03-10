990d9f6ece667c6dc20d75b507e83c186c1b6760
Punam Woosaree <woosaree@ualberta.ca>
2019-03-10 16:47:18 -0600

    Filter view implemented using react-native-sectioned-multi-select

--- walkandtalk/src/Router.js
+++ walkandtalk/src/Router.js
@@ -19,7 +19,10 @@ import FormScreen from "./components/form/FormScreen";
 const RouterComponent = () => (
   <Router>
     <Stack key="root">
-    
+    <Stack key="auth" hideNavBar>
+      <Scene key="login" component={AuthScreen} />
+      <Scene key="signup" component={SignupScreen} />
+    </Stack>
       <Stack key="app" hideNavBar panHandlers={null}>
         <Tabs showLabel={false}>
           <Scene
--- walkandtalk/src/components/search/SearchScreen.js
+++ walkandtalk/src/components/search/SearchScreen.js
@@ -5,7 +5,8 @@ import {
   TextInput,
   StyleSheet,
   TouchableHighlight,
-  Alert
+  Alert,
+  Dimensions,
 } from "react-native";
 import Ionicon from "react-native-vector-icons/Ionicons";
 import Icon from 'react-native-vector-icons/MaterialIcons'
@@ -24,8 +25,9 @@ import {
   Content
 } from "native-base";
 
-const icon = require("../../assets/icons/search.png");
+const searchIcon = require("../../assets/icons/search.png");
 const icon2 = require("../../assets/icons/form.png");
+
 
 const items = [
   {
@@ -69,16 +71,92 @@ class SearchScreen extends Component {
     super()
     this.state = {
       selectedItems: [],
-      text:""
+      text:"",
+      currentFilters:null
     }
   }
   onSelectedItemsChange = (selectedItems) => {
     this.setState({ selectedItems });
   }
 
+  onConfirm = () => {
+    this.setState({ currentFilters: this.state.selectedItems })
+  }
+
 
   render() {
+    filterHeader = <Header
+      style={ScreenStyleSheet.header}
+      androidStatusBarColor="#A680B8"
+      androidStatusBarStyle="light-content"
+    >
+      <Body style={ScreenStyleSheet.headerBody}>
+        <Title>Filter</Title>
+      </Body>
+    </Header>
+
+
+    filterPopup =
+    <View>
+     <SectionedMultiSelect
+       items={items}
+       uniqueKey='id'
+       subKey='children'
+       iconKey='icon'
+       headerComponent = {filterHeader}
+       confirmText = "Select"
+       onConfirm={this.onConfirm}
+       showChips={true}
+       hideSearch={true}
+       showDropDowns={false}
+       readOnlyHeadings={true}
+       onSelectedItemsChange={this.onSelectedItemsChange}
+       selectedItems={this.state.selectedItems}
+       colors={{ primary: this.state.selectedItems.length ? "#A680B8" : 'crimson',}}
+       styles={{
+          selectedItemText: {
+            color: "#A680B8",
+          },
+          selectedSubItemText: {
+             color: "#A680B8",
+          },
+          chipText: {
+             maxWidth: Dimensions.get('screen').width - 90,
+          },
+          chipWrapper:{
+            marginBottom:15,
+            backgroundColor:"red"
+          },
+          container:{
+            marginBottom: 250
+          },
+          selectToggle:{
+            width:40,
+            height:40,
+            marginBottom:15,
+            marginTop:10,
+            borderWidth:1,
+            borderColor: "grey",
+            borderTopRightRadius: 3,
+            borderBottomRightRadius: 3,
+            marginRight:5,
+            paddingTop:5,
+            backgroundColor: "#A680B8",
+            alignSelf:"flex-end"
+          }
+        }}
+         cancelIconComponent={
+          <Icon
+           size={20}
+           name="close"
+           style={{ color: 'white' }}
+         />
+         }
+     />
+     </View>
+
     return (
+
       <Container>
         <Header
           style={ScreenStyleSheet.header}
@@ -90,41 +168,7 @@ class SearchScreen extends Component {
           </Body>
         </Header>
 
-      <View>
-       <SectionedMultiSelect
-         items={items}
-         uniqueKey='id'
-         subKey='children'
-         iconKey='icon'
-         selectText='Search for events...'
-         confirmText = "Select"
-         hideSearch={true}
-         showDropDowns={false}
-         readOnlyHeadings={true}
-         onSelectedItemsChange={this.onSelectedItemsChange}
-         selectedItems={this.state.selectedItems}
-         colors={{
-           primary:{
-             color: "#A680B8"
-           }
-         }}
-         styles={{
-            selectedItemText: {
-              color: "#A680B8",
-            },
-            selectedSubItemText: {
-               color: "#A680B8",
-            }
-          }}
-           cancelIconComponent={
-            <Icon
-             size={20}
-             name="close"
-             style={{ color: 'white' }}
-           />
-           }
-       />
-       </View>
+
 
         <Content contentContainerStyle={ScreenStyleSheet.content}>
           {/* Search bar */}
@@ -136,8 +180,10 @@ class SearchScreen extends Component {
               style={styles.placeHolder}
               onChangeText={text => this.setState({ text })}
             />
-            <Entypo name="sound-mix" style={styles.filterIcon} />
           </View>
+          {filterPopup}
+
+
           <View style={ScreenStyleSheet.lineSeparator} />
           <Text style={ScreenStyleSheet.sectionTitle}>Events near you</Text>
           <BaseCard
@@ -154,15 +200,34 @@ class SearchScreen extends Component {
 const styles = StyleSheet.create({
   box: {
     height: 40,
+    width:Dimensions.get('screen').width - 70,
+    borderWidth: 1,
+    borderTopLeftRadius: 3,
+    borderBottomLeftRadius: 3,
+    borderColor: "grey",
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    marginBottom: 10,
+    marginTop:10,
+    alignSelf:"flex-start",
+    position:"absolute"
+  },
+  boxWithFilter: {
+    height: 40,
     borderWidth: 1,
     borderRadius: 3,
     borderColor: "grey",
-    justifyContent: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingHorizontal: 15,
     flexDirection: "row",
     marginBottom: 10
   },
   searchIcon: {
+    marginTop:3,
+    marginRight:3,
     fontSize: 25
   },
   placeHolder: {
