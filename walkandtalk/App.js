92e9796e47629b9fef8ed1cce2829d63559bf2ec
Skryt <cokan@ualberta.ca>
2019-03-19 16:57:27 -0600

    no more warnings removed react-nav react-native-nav

--- walkandtalk/App.js
+++ walkandtalk/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
-import firebase from "@firebase/app";
-import "@firebase/auth";
+
 import Main from "./src/Main";
 
 export default class App extends Component {
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyCCL5OqXeMa4iAa-xEcwtOkL3bwbWReuM4",
-      authDomain: "walkandtalk401.firebaseapp.com",
-      databaseURL: "https://walkandtalk401.firebaseio.com",
-      projectId: "walkandtalk401",
-      storageBucket: "walkandtalk401.appspot.com",
-      messagingSenderId: "592772538333"
-    });
-  }
   render() {
     return <Main />;
   }
