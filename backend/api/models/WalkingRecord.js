fd6fd8fcf4de2a45d585c6e4dff5668e09bab8ea
skryt <cokan@ualberta.ca>
2019-03-20 14:38:11 -0600

    revert walking record

--- backend/api/models/WalkingRecord.js
+++ backend/api/models/WalkingRecord.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../../config/database");
-const WalkingEvent = require("./WalkingEvent");
 
 // table name
 const tableName = "walkingrecords";
@@ -12,6 +11,14 @@ const WalkingRecord = sequelize.define(tableName, {
     autoIncrement: true,
     primaryKey: true
   },
+  organizer: Sequelize.STRING,
+  fullname: Sequelize.STRING,
+  title: Sequelize.STRING,
+  email: Sequelize.STRING,
+  venue: Sequelize.STRING,
+  date: Sequelize.STRING,
+  start_time: Sequelize.STRING,
+  end_time: Sequelize.STRING,
   location: Sequelize.STRING,
   distance: Sequelize.INTEGER,
   duration: Sequelize.INTEGER,
@@ -23,8 +30,6 @@ const WalkingRecord = sequelize.define(tableName, {
   completed: Sequelize.INTEGER,
   total_attendees: Sequelize.INTEGER
 });
-
-WalkingRecord.hasOne(WalkingEvent);
 
 // export walking record
 module.exports = WalkingRecord;
