ef3b4395a730cc417bb846c27049163f624ddff3
Punam Woosaree <woosaree@ualberta.ca>
2019-03-21 01:57:07 -0600

    search implemented better, still view needs to be adjusted

--- walkandtalk/android/app/src/main/java/com/walkandtalk/MainApplication.java
+++ walkandtalk/android/app/src/main/java/com/walkandtalk/MainApplication.java
@@ -30,11 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-<<<<<<< HEAD
             new RNCWebViewPackage(),
-=======
             new RNGooglePlacesPackage(),
->>>>>>> eventLocation
             new ImagePickerPackage(),
           new DatePickerPackage(),
           new VectorIconsPackage(),
